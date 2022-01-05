9aec0e4a469bdc4f5101f29d53c6cc026bc1044a
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-01-05 18:42:09 +0900

    calenderメソッドを右寄せに変更、dayの条件判定を削除、不適切な引数名を変更、例外処理を削除

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -4,68 +4,39 @@ require "optparse"
 options = ARGV.getopts("m:","y:")
 option_month = options["m"]
 option_year = options["y"]
-
 today = Date.today
-tomonth = today.month
-toyear = today.year
+month = options["m"] || today.month
+year = options["y"] || today.year
 weeks = Date.today.wday
 
-def calender(i,o,days,beginning)
-  months_years =  "#{i}月 #{o}"
+def calender(month,year,days,beginning)
+  months_years = "#{month}月 #{year}"
   count = 0
-  if o.to_i >= 10000
-    puts "year #{o.to_i} not in range 1..9999"
-  else
-    puts months_years.center(20)
-    puts "日 月 火 水 木 金 土"
-    beginning.times do|space|
-      print "   "
-      count += 1
-    end
-    (1..days.to_i+ 1).each do |day|
-      count += 1
-      if day < 10 && count % 7 == 0
-        puts day.to_s.center(3)
-      elsif day < 10
-        print day.to_s.center(3)
-      elsif day >= 10 && count % 7 == 0
-        puts day.to_s.center(3)
-      else
-        print day.to_s.center(3)
-      end
-    end
-    puts " "
-    puts " "
+  puts months_years.center(20)
+  puts "日 月 火 水 木 金 土"
+  beginning.times do|space|
+    print "   "
+    count += 1
   end
+  (1..days.to_i + 1).each do |day|
+    count += 1
+    if count % 7 == 0
+      puts day.to_s.rjust(2)
+    else
+      print day.to_s.rjust(2) + " "
+    end
+  end
+  puts " "
+  puts " "
 end
-
-
 if option_month.to_i >= 13 || option_month == "0"
-  puts "#{option_month.to_i} is neither a month number (1..12) nor a name"
-else
-  if option_month || option_year 
-    option_month ||= tomonth
-    option_year ||= toyear
-    firstday = Date.new(option_year.to_i,option_month.to_i,1)
-    lastday = Date.new(option_year.to_i,option_month.to_i,-1)
-  else
-    option_month ||= tomonth
-    option_year ||= toyear
-    firstday = Date.new(toyear,tomonth,1)
-    lastday = Date.new(toyear,tomonth,-1)
-  end
+  raise "#{option_month.to_i} is neither a month number (1..12) nor a name"
+end
+option_month ||= month
+option_year ||= year
+firstday = Date.new(year.to_i,month.to_i,1)
+lastday = Date.new(year.to_i,month.to_i,-1)
+days = lastday - firstday
+beginning = firstday.wday.to_i
 
-  days = lastday - firstday
-  beginning = firstday.wday.to_i
-
-  if option_month && option_year
-    calender(option_month,option_year,days,beginning)
-  elsif option_month 
-    calender(option_month,toyear,days,beginning)
-  elsif opyear
-    calender(tomonth,option_year,days,beginning)
-  else
-    calender(tomonth,toyear,days,beginning)
- end
-end 
-
+calender(month,year,days,beginning)
