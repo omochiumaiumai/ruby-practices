21808168e537f1ae7b4a17a536c86f0bfe55ea3e
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-01-13 18:24:11 +0900

    rubocopに指摘された部分を修正、不要なコメントを削除

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,41 +1,41 @@
-score = ARGV[0] #引数取得
-scores = score.split(",")
+# frozen_string_literal: true
+
+score = ARGV[0]
+scores = score.split(',')
 shots = []
 scores.each do |s|
-  if s == "X" #strike
+  if s == 'X'
     shots << 10
     shots << 0
   else
-    shots << s.to_i #足し算するために数字に変換
+    shots << s.to_i
   end
 end
 
 frames = []
-shots.each_slice(2) do |s| #フレーム毎に分割
+shots.each_slice(2) do |s|
   frames << s
 end
 
-def strike_score_plus(frames,count)
+def strike_score_plus(frames, count)
   if frames[count][0] == 10
-      10 + 10 + frames[count + 1][0]
-    else
-      10 + frames[count].sum
-    end
+    10 + 10 + frames[count + 1][0]
+  else
+    10 + frames[count].sum
+  end
 end
 
 point = 0
 count = 0
 frames.each do |frame|
   count += 1
-  print frame
-  print "#{count}回目"
-  if frame[0] == 10 && count < 10 # strike
-    point += strike_score_plus(frames,count)
-  elsif frame.sum == 10 && count < 10 # spare
-    point += (frame.sum + frames[count][0])
-  else
-    point += frame.sum
-  end
-  puts point
+  point +=
+    if frame[0] == 10 && count < 10
+      strike_score_plus(frames, count)
+    elsif frame.sum == 10 && count < 10
+      (frame.sum + frames[count][0])
+    else
+      frame.sum
+    end
 end
 puts point
