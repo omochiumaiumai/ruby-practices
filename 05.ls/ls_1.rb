10ee74d5887053416a0fd63e073e9f1dea6fdccf
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-03-10 21:07:15 +0900

    変数名files_transposedをtransposed_filesに変更

--- 05.ls/ls_1.rb
+++ 05.ls/ls_1.rb
@@ -12,20 +12,20 @@ quotient += 1 if surplus.positive?
 slice_files = file_names.each_slice(quotient).to_a
 max_array_size = slice_files.map(&:size).max
 
-files_transposed = slice_files.map { |file| (file + [' '] * max_array_size)[0...max_array_size] }.transpose # 配列の要素数を揃える
-columns_max_string = files_transposed.transpose.map { |array| array.sort_by(&:length).last.length }
+transposed_files = slice_files.map { |file| (file + [' '] * max_array_size)[0...max_array_size] }.transpose # 配列の要素数を揃える
+columns_max_string = transposed_files.transpose.map { |array| array.sort_by(&:length).last.length }
 
 line_one = []
-files_transposed.each do |array|
-  line_one << array[0].ljust(*columns_max_string[0], ' ')
+transposed_files.each do |array|
+  line_one << array[0].ljust(columns_max_string[0], ' ')
 end
 line_two = []
-files_transposed.each do |array|
-  line_two << array[1].ljust(*columns_max_string[1], ' ')
+transposed_files.each do |array|
+  line_two << array[1].ljust(columns_max_string[1], ' ')
 end
 line_three = []
-files_transposed.each do |array|
-  line_three << array[2].ljust(*columns_max_string[2], ' ')
+transposed_files.each do |array|
+  line_three << array[2].ljust(columns_max_string[2], ' ')
 end
 
 line_one.zip(line_two, line_three) { |arary| puts arary.join(' ' * countfile) }
