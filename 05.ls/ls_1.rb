615d39c50ee27fb4b80fcec543df5dc323d2fb6f
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-05-13 18:14:40 +0900

    出力メソッドoutputを定義

--- 05.ls/ls_1.rb
+++ 05.ls/ls_1.rb
@@ -11,15 +11,31 @@ class DirSelect
   attr_reader :file_names
 end
 
+def output(options)
+  if options['l']
+    puts "total #{@file_blocks.sum}"
+    @files_zip.each do |array|
+      files_status = array.map.with_index do |item, column_index|
+        column_index == 7 ? item.ljust(@status_columns_maxstring[column_index]) : item.rjust(@status_columns_maxstring[column_index])
+      end
+      puts files_status.join(' ' * 2)
+    end
+  else
+    @transposed_files.each do |array|
+      ljusted_files = array.map.with_index { |item, column_index| item.ljust(@columns_max_string[column_index]) }
+      puts ljusted_files.join(' ' * @countfile)
+    end
+  end
+end
+
 options = ARGV.getopts('a', 'r', 'l')
 
 option = DirSelect.new
 option.option_select(options)
-
 file_names = option.file_names
 status = option.file_names.map { |file| File.lstat(file) }
 
-countfile = file_names.map(&:length).max # 最大文字数を算出
+@countfile = file_names.map(&:length).max # 最大文字数を算出
 files_count = file_names.count
 quotient, surplus = files_count.divmod(3) # 商と余り
 quotient += 1 if surplus.positive?
@@ -27,8 +43,8 @@ quotient += 1 if surplus.positive?
 slice_files = file_names.each_slice(quotient).to_a
 max_array_size = slice_files.map(&:size).max
 
-transposed_files = slice_files.map { |file| (file + [' '] * max_array_size)[0...max_array_size] }.transpose # 配列の要素数を揃える
-columns_max_string = transposed_files.transpose.map { |array| array.max_by(&:length).length }
+@transposed_files = slice_files.map { |file| (file + [' '] * max_array_size)[0...max_array_size] }.transpose # 配列の要素数を揃える
+@columns_max_string = @transposed_files.transpose.map { |array| array.max_by(&:length).length }
 
 # パーミッション
 TYPES = { 'file' => '-', 'directory' => 'd', ' characterSpecial' => 'c', 'blockSpecial' => 'b', 'link' => 'l', 'fifo' => 'p', 'socket' => 's' }.freeze
@@ -49,26 +65,11 @@ groups = file_names.map { |file| Etc.getgrgid(File.lstat(file).gid).name }
 file_size = status.map { |file| file.size.to_s }
 update_day = status.map { |time| time.mtime.strftime('%-m %e') }
 update_time = status.map { |time| time.mtime.strftime('%H:%M') }
-file_blocks = status.map(&:blocks)
+@file_blocks = status.map(&:blocks)
 
 # 結合
-files_zip = files_permission.zip(hard_links, user, groups, file_size, update_day, update_time, file_names)
-status_columns_maxstring = files_zip.transpose.map { |array| array.max_by(&:length).length }
+@files_zip = files_permission.zip(hard_links, user, groups, file_size, update_day, update_time, file_names)
+@status_columns_maxstring = @files_zip.transpose.map { |array| array.max_by(&:length).length }
 
 # 出力
-if options['l']
-  puts "total #{file_blocks.sum}"
-  files_zip.each do |array|
-    files_status = array.map.with_index do |item, column_index|
-      column_index == 7 ? item.ljust(status_columns_maxstring[column_index]) : item.rjust(status_columns_maxstring[column_index])
-    end
-    puts files_status.join(' ' * 2)
-  end
-end
-
-unless options['l']
-  transposed_files.each do |array|
-    ljusted_files = array.map.with_index { |item, column_index| item.ljust(columns_max_string[column_index]) }
-    puts ljusted_files.join(' ' * countfile)
-  end
-end
+output(options)
