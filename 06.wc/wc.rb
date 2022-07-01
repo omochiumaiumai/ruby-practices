16f4d7b3628d79b7ff67ab05cea3a070d700192e
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-07-01 23:16:41 +0900

    prepare_outputメソッドから整形の処理をoutputへ変更
    outputメソッドの出力処理をハッシュから配列での処理に変更

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -32,54 +32,55 @@ def calculate_values(strings_in_files)
     files.map(&:bytesize).sum
   end
 
-  lines.zip(words, byte_size)
+  values = lines.zip(words, byte_size).transpose
+  { line_count: values[0], word_count: values[1], character_count: values[2] }
 end
 
-def options_select(values, options)
-  return values if !options['c'] && !options['w'] && !options['l']
+def options_select(values_hash, options)
+  return values_hash if !options['c'] && !options['w'] && !options['l']
 
-  values_hash = { line_count: values[0], word_count: values[1], character_count: values[2] }
   values_hash.delete(:character_count) unless options['c']
   values_hash.delete(:word_count) unless options['w']
   values_hash.delete(:line_count) unless options['l']
-  values_hash.values
+  values_hash
 end
 
-def prepare_output(calculated_values, file_names, options)
-  total_values = calculated_values.transpose
-  calculated_values.map! do |values|
-    values.map { |value| value.to_s.rjust(8, ' ') }
-  end
-  calculated_values = calculated_values.transpose
+def prepare_output(calculated_values, options)
   calculated_values = options_select(calculated_values, options)
+  total_values = calculated_values.values.map(&:sum)
+  calculated_values[:total_values] = total_values
+  calculated_values
+end
 
-  total_values.map!(&:sum)
-  total_values.map! { |value| value.to_s.rjust(8, ' ') }
-  total_values = options_select(total_values, options)
-  total_values += [' ', 'total']
+def output(values_and_total_values)
+  values = values_and_total_values.each_value do |array|
+    array.map! { |value| value.to_s.rjust(8, ' ') }
+    array.join
+  end
+
+  file_names = collect_file_names
   max_length = file_names.max.length
   file_names.map! { |file_name| file_name.ljust(max_length, ' ') }
 
-  calculated_values = calculated_values.push(file_names).transpose
-  calculated_values.map! do |values|
-    values.insert(-2, ' ')
-    values.join
-  end
-  { calculated_values: calculated_values, total_values: total_values.join }
-end
+  total_values = values[:total_values]
+  total_values += [' ', 'total']
+  values.delete(:total_values)
+  processed_values = values.values.push(file_names)
 
-def output(values_and_filenames)
-  puts values_and_filenames[:calculated_values]
-  puts values_and_filenames[:total_values] if values_and_filenames[:calculated_values].count > 1
+  output = processed_values.transpose.map do |array|
+    array.insert(-2, ' ')
+    array.join
+  end
+  puts output
+  puts total_values.join if output.count > 1
 end
 
 def main
   options = ARGV.getopts('c', 'l', 'w')
   strings_in_files = check_input_source
   calculated_values = calculate_values(strings_in_files)
-  file_names = collect_file_names
-  values_and_filenames = prepare_output(calculated_values, file_names, options)
-  output(values_and_filenames)
+  values_and_total_values = prepare_output(calculated_values, options)
+  output(values_and_total_values)
 end
 
 main
