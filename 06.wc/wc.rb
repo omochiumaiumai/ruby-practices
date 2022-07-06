d036d7512c5b144ae0e8e0bdd27709f245f3405f
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-07-06 15:05:05 +0900

    情報取得と出力方法の変更

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -19,58 +19,68 @@ def collect_file_names
   end
 end
 
-def calculate_values(strings_in_files, options)
+def calculate_values(strings_in_files)
+  file_names = collect_file_names
+
   strings_in_files.map! do |files|
     files.map { |file| file.delete("\n") }
   end
 
   lines = strings_in_files.map(&:count)
+
   words = strings_in_files.map do |files|
     files.map { |file| file.split.count }.sum
   end
-  byte_size = strings_in_files.map do |files|
+
+  character_count = strings_in_files.map do |files|
     files.map(&:bytesize).sum
   end
 
-  values = lines.zip(words, byte_size).transpose
-  values_and_total_values = { line_count: values[0], word_count: values[1], character_count: values[2] }
-  options_select(values_and_total_values, options)
-end
-
-def options_select(values_hash, options)
-  return values_hash if !options['c'] && !options['w'] && !options['l']
-
-  values_hash.delete(:character_count) unless options['c']
-  values_hash.delete(:word_count) unless options['w']
-  values_hash.delete(:line_count) unless options['l']
-  values_hash
-end
-
-def output(values_and_total_values)
-  total_values = values_and_total_values.values.map(&:sum)
-  values = values_and_total_values.each_value do |array|
-    array.map! { |value| value.to_s.rjust(8, ' ') }
-    array.join
+  values = file_names.zip(lines, words, character_count)
+  values.map do |x|
+    { file_name: x[0], line_count: x[1], word_count: x[2], character_count: x[3] }
   end
+end
 
+# def options_select(values_hash, options)
+#  return values_hash if !options['c'] && !options['w'] && !options['l']
+
+#  values_hash.delete(:character_count) unless options['c']
+#  values_hash.delete(:word_count) unless options['w']
+#  values_hash.delete(:line_count) unless options['l']
+#  values_hash
+# end
+
+def output(values, options)
   file_names = collect_file_names
   max_length = file_names.max.length
-  file_names.map! { |file_name| file_name.ljust(max_length, ' ') }
+  values_and_filenames = values.map(&:values).transpose
+  total_values = { line_total: values_and_filenames[1].sum, words_total: values_and_filenames[2].sum, character_total: values_and_filenames[3].sum }
 
-  processed_values = values.values.push(file_names)
-
-  processed_values.transpose.map do |array|
-    array.insert(-2, ' ')
-    puts array.join
+  values.each do |values_hash|
+    if options['l']
+      p "#{values_hash[:line_count].to_s.rjust(8, ' ')} #{values_hash[:file_name].ljust(max_length, '0')}"
+    elsif options['w']
+      p "#{values_hash[:word_count].to_s.rjust(8, ' ')} #{values_hash[:file_name].ljust(max_length, '0')}"
+    elsif options['c']
+      p "#{values_hash[:character_count].to_s.rjust(8, ' ')} #{values_hash[:file_name].ljust(max_length, '0')}"
+    end
   end
-  puts "#{total_values.map { |array| array.to_s.rjust(8, ' ') }.join} total" if file_names.count > 1
+
+  if options['l'] && file_names.count > 1
+    p "#{total_values[:line_total].to_s.rjust(8, ' ')} total"
+  elsif options['w'] && file_names.count > 1
+    p "#{total_values[:words_total].to_s.rjust(8, ' ')} total"
+  elsif options['c'] && file_names.count > 1
+    p "#{total_values[:character_total].to_s.rjust(8, ' ')} total"
+  end
 end
 
 def main
   options = ARGV.getopts('c', 'l', 'w')
   strings_in_files = check_input_source
-  values_and_total_values = calculate_values(strings_in_files, options)
-  output(values_and_total_values)
+  values = calculate_values(strings_in_files)
+  output(values, options)
 end
 
 main
