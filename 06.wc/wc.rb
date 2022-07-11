2773b6ac53b1fbfe94684eb4848af4627d037c29
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-07-11 15:13:31 +0900

    collect_file_namesメソッドを削除、check_input_sourceメソッドの戻り値を変更
    calculate_valuesメソッドの各値の取得方法を変更
    
    outputメソッドの出力方法を変更

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -5,82 +5,57 @@ require 'optparse'
 
 def check_input_source
   if File.pipe?($stdin)
-    [$stdin.to_a]
+    [{ file_name: '', content: [$stdin.to_a] }]
   else
-    ARGV.map { |file| File.open(file, 'r').readlines }
+    file_names = ARGV.map { |file_name| " #{file_name}" }
+    [{ file_name: file_names, content: ARGV.map { |file| File.open(file, 'r').readlines } }]
   end
 end
 
-def collect_file_names
-  if ARGV.any?
-    ARGV
-  else
-    ['']
-  end
-end
-
-def calculate_values(strings_in_files)
-  file_names = collect_file_names
-
-  strings_in_files.map! do |files|
-    files.map { |file| file.delete("\n") }
-  end
-
-  lines = strings_in_files.map(&:count)
-
-  words = strings_in_files.map do |files|
-    files.map { |file| file.split.count }.sum
-  end
-
-  character_count = strings_in_files.map do |files|
-    files.map(&:bytesize).sum
-  end
-
-  values = file_names.zip(lines, words, character_count)
-  values.map do |x|
-    { file_name: x[0], line_count: x[1], word_count: x[2], character_count: x[3] }
-  end
-end
-
-# def options_select(values_hash, options)
-#  return values_hash if !options['c'] && !options['w'] && !options['l']
-
-#  values_hash.delete(:character_count) unless options['c']
-#  values_hash.delete(:word_count) unless options['w']
-#  values_hash.delete(:line_count) unless options['l']
-#  values_hash
-# end
-
-def output(values, options)
-  file_names = collect_file_names
-  max_length = file_names.max.length
-  values_and_filenames = values.map(&:values).transpose
-  total_values = { line_total: values_and_filenames[1].sum, words_total: values_and_filenames[2].sum, character_total: values_and_filenames[3].sum }
-
-  values.each do |values_hash|
-    if options['l']
-      p "#{values_hash[:line_count].to_s.rjust(8, ' ')} #{values_hash[:file_name].ljust(max_length, '0')}"
-    elsif options['w']
-      p "#{values_hash[:word_count].to_s.rjust(8, ' ')} #{values_hash[:file_name].ljust(max_length, '0')}"
-    elsif options['c']
-      p "#{values_hash[:character_count].to_s.rjust(8, ' ')} #{values_hash[:file_name].ljust(max_length, '0')}"
+def calculate_values(filename_and_contents)
+  filename_and_contents.map do |hash|
+    lines = hash[:content].map do |contents|
+      contents.map { |content| content.delete("\n") }.count
     end
+    words = hash[:content].map do |contents|
+      contents.map { |content| content.split.count }.sum
+    end
+    character_count = hash[:content].map do |contents|
+      contents.map(&:bytesize).sum
+    end
+    { file_names: hash[:file_name], line_count: lines, word_count: words, character_count: character_count }
   end
+end
 
-  if options['l'] && file_names.count > 1
-    p "#{total_values[:line_total].to_s.rjust(8, ' ')} total"
-  elsif options['w'] && file_names.count > 1
-    p "#{total_values[:words_total].to_s.rjust(8, ' ')} total"
-  elsif options['c'] && file_names.count > 1
-    p "#{total_values[:character_total].to_s.rjust(8, ' ')} total"
+def output(values_and_filenames, options)
+  values_and_filenames = values_and_filenames[0]
+
+  line_option = options['l']
+  word_option = options['w']
+  character_count_option = options['c']
+
+  file_count = values_and_filenames[:file_names].count
+
+  file_count.times do |count|
+    print values_and_filenames[:line_count][count].to_s.rjust(8) if line_option
+    print values_and_filenames[:word_count][count].to_s.rjust(8) if word_option
+    print values_and_filenames[:character_count][count].to_s.rjust(8) if character_count_option
+    print (values_and_filenames[:file_names][count]).to_s
+
+    puts ''
+
+    print values_and_filenames[:line_count].sum.to_s.rjust(8) if line_option && count > 1
+    print values_and_filenames[:word_count].sum.to_s.rjust(8) if word_option && count > 1
+    print values_and_filenames[:character_count].sum.to_s.rjust(8) if character_count_option && count > 1
+    print ' total' if count > 1
   end
 end
 
 def main
   options = ARGV.getopts('c', 'l', 'w')
-  strings_in_files = check_input_source
-  values = calculate_values(strings_in_files)
-  output(values, options)
+  filename_and_contents = check_input_source
+  values_and_filenames  = calculate_values(filename_and_contents)
+  output(values_and_filenames, options)
 end
 
 main
