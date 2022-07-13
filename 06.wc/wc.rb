eac6481c22da70a3dcca4e672693f73d87c22384
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-07-13 22:35:26 +0900

    各オプションの条件分岐メソッドを追加、output_totalメソッドをoutputメソッド内で動かす形に変更

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -28,32 +28,44 @@ def calculate_values(filename_and_contents)
 end
 
 def display_line?(options)
-  (!options['c'] && !options['w'] && !options['l'])
+  (!options['c'] && !options['w'] && !options['l']) || options['l']
+end
+
+def display_word?(options)
+  (!options['c'] && !options['w'] && !options['l']) || options['w']
+end
+
+def display_character?(options)
+  (!options['c'] && !options['w'] && !options['l']) || options['c']
 end
 
 def output(values_and_filenames, options)
   values_and_filenames = values_and_filenames[0]
   max_length = values_and_filenames[:file_names].map { _1.length }.max
   file_count = values_and_filenames[:file_names].count
+
   display_line = display_line?(options)
+  display_word = display_word?(options)
+  display_character = display_character?(options)
 
   file_count.times do |count|
-    print values_and_filenames[:line_count][count].to_s.rjust(8) if options['l'] || display_line
-    print values_and_filenames[:word_count][count].to_s.rjust(8) if options['w'] || display_line
-    print values_and_filenames[:character_count][count].to_s.rjust(8) if options['c'] || display_line
+    print values_and_filenames[:line_count][count].to_s.rjust(8) if display_line
+    print values_and_filenames[:word_count][count].to_s.rjust(8) if display_word
+    print values_and_filenames[:character_count][count].to_s.rjust(8) if display_character
     print values_and_filenames[:file_names][count].ljust(max_length)
     puts ''
   end
+  output_total(values_and_filenames, options, max_length) if file_count > 1
 end
 
-def output_total(values_and_filenames, options)
-  values_and_filenames = values_and_filenames[0]
+def output_total(values_and_filenames, options, max_length)
   display_line = display_line?(options)
-  max_length = values_and_filenames[:file_names].map { _1.length }.max
+  display_word = display_word?(options)
+  display_character = display_character?(options)
 
-  print values_and_filenames[:line_count].sum.to_s.rjust(8) if options['l'] || display_line
-  print values_and_filenames[:word_count].sum.to_s.rjust(8) if options['w'] || display_line
-  print values_and_filenames[:character_count].sum.to_s.rjust(8) if options['c'] || display_line
+  print values_and_filenames[:line_count].sum.to_s.rjust(8) if display_line
+  print values_and_filenames[:word_count].sum.to_s.rjust(8) if display_word
+  print values_and_filenames[:character_count].sum.to_s.rjust(8) if display_character
   print ' total'.ljust(max_length)
 end
 
@@ -62,7 +74,6 @@ def main
   filename_and_contents = check_input_source
   values_and_filenames  = calculate_values(filename_and_contents)
   output(values_and_filenames, options)
-  output_total(values_and_filenames, options) if values_and_filenames[0][:file_names].count > 1
 end
 
 main
