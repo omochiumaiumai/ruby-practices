b4a4b77938ca9af165800d1784f8deaa8b615d1c
omochiumaiumai <shu.nakazato0419@gmail.com>
2023-03-11 21:00:43 +0900

    initializeでshotクラスのscoreメソッドを呼び出さないように修正

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -6,20 +6,20 @@ class Frame
   attr_reader :first_shot, :second_shot
 
   def initialize(first_mark, second_mark, third_mark = nil)
-    @first_shot = Shot.new(first_mark).score
-    @second_shot = Shot.new(second_mark).score
-    @third_shot = Shot.new(third_mark).score
+    @first_shot = Shot.new(first_mark)
+    @second_shot = Shot.new(second_mark)
+    @third_shot = Shot.new(third_mark)
   end
 
   def score
-    @first_shot + @second_shot + @third_shot
+    @first_shot.score + @second_shot.score + @third_shot.score
   end
 
   def strike?
-    @first_shot == 10
+    @first_shot.score == 10
   end
 
   def spare?
-    @first_shot + @second_shot == 10
+    @first_shot.score + @second_shot.score == 10
   end
 end
