6749d4b312f0539913fdbae7ce5b7c1bf05611c1
omochiumaiumai <shu.nakazato0419@gmail.com>
2023-03-16 01:58:25 +0900

    initializeでShotクラスのインスタンスを受け取るように修正/scoreメソッドを修正

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -6,13 +6,15 @@ class Frame
   attr_reader :first_shot, :second_shot
 
   def initialize(first_mark, second_mark, third_mark = nil)
-    @first_shot = Shot.new(first_mark)
-    @second_shot = Shot.new(second_mark)
-    @third_shot = Shot.new(third_mark)
+    @first_shot = first_mark
+    @second_shot = second_mark
+    @third_shot = third_mark
   end
 
   def score
-    @first_shot.score + @second_shot.score + @third_shot.score
+    score = @first_shot.score + @second_shot.score
+    score += @third_shot.score if @third_shot
+    score
   end
 
   def strike?
