5b1411a26ac66da1c438f58e9dd80af4c881c9c9
omochiumaiumai <shu.nakazato0419@gmail.com>
2023-04-02 17:58:08 +0900

    呼び出すクラス名を変更/出力の処理を追加

--- 08.ls_object/main.rb
+++ 08.ls_object/main.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-require_relative 'file'
-require_relative 'directory'
+require 'optparse'
+require_relative 'my_file'
 require_relative 'option'
 require_relative 'ls_command'
+
+options = ARGV.getopts('a', 'r', 'l')
+LsCommand.new(options).output_files
