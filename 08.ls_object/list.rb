cd0af305bf5e80a09fba4cadafb6d059a342db6c
omochiumaiumai <shu.nakazato0419@gmail.com>
2023-04-19 16:02:34 +0900

    Listクラスを作成/メソッドを切り出し

---
+++ 08.ls_object/list.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class List
+  def initialize(files)
+    @files = files
+  end
+
+  def lists_column_max_width
+    max_permissions = @files.map { |file| file.permission.length }.max
+    max_hard_links = @files.map { |file| file.hard_link.to_s.length }.max
+    max_owner = @files.map { |file| file.owner_name.length }.max
+    max_group = @files.map { |file| file.group_name.length }.max
+    max_size = @files.map { |file| file.size.to_s.length }.max
+    max_date = @files.map { |file| file.update_day.length }.max
+    max_time = @files.map { |file| file.update_time.length }.max
+    { permission: max_permissions, hard_link: max_hard_links, owner: max_owner,
+      group: max_group, size: max_size, date: max_date, time: max_time }
+  end
+
+  def total_blocks
+    @files.map(&:blocks).sum
+  end
+
+  def create_columns
+    columns = []
+    files_per_column = (@files.count / 3.0).ceil
+    @files.each_slice(files_per_column) { |file| columns << file }
+    columns.last << '' while columns.last.size < files_per_column
+
+    columns.transpose
+  end
+
+  def columns_max_width
+    columns = create_columns
+    columns_max_width = []
+    columns.each do |column|
+      column_widths = column.map do |file|
+        if file.instance_of?(FileAndDirectory)
+          file.name.length + 1
+        else
+          file.to_s.length
+        end
+      end
+      columns_max_width << column_widths
+    end
+    columns_max_width.transpose.map(&:max)
+  end
+end
--- 08.ls_object/ls_command.rb
+++ 08.ls_object/ls_command.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'list'
+
 class LsCommand
   def initialize(options)
     @options = Option.new(options)
@@ -16,53 +18,14 @@ class LsCommand
     files
   end
 
-  def lists_column_max_width(files)
-    max_permissions = files.map { |file| file.permission.length }.max
-    max_hard_links = files.map { |file| file.hard_link.to_s.length }.max
-    max_owner = files.map { |file| file.owner_name.length }.max
-    max_group = files.map { |file| file.group_name.length }.max
-    max_size = files.map { |file| file.size.to_s.length }.max
-    max_date = files.map { |file| file.update_day.length }.max
-    max_time = files.map { |file| file.update_time.length }.max
-    { permission: max_permissions, hard_link: max_hard_links, owner: max_owner,
-      group: max_group, size: max_size, date: max_date, time: max_time }
-  end
-
-  def get_total_blocks(files)
-    files.map(&:blocks).sum
-  end
-
-  def create_columns(files)
-    columns = []
-    files_per_column = (files.count / 3.0).ceil
-    files.each_slice(files_per_column) { |file| columns << file }
-    columns.last << '' while columns.last.size < files_per_column
-
-    columns.transpose
-  end
-
-  def columns_max_width(columns)
-    columns_max_width = []
-    columns.each do |column|
-      column_widths = column.map do |file|
-        if file.instance_of?(FileAndDirectory)
-          file.name.length + 1
-        else
-          file.to_s.length
-        end
-      end
-      columns_max_width << column_widths
-    end
-    columns_max_width.transpose.map(&:max)
-  end
-
   def output_files
     files = select_files
-    columns_max_width = lists_column_max_width(files)
-
+    files_list = List.new(files)
     if @options.long
-      puts "total #{get_total_blocks(files)}"
+      columns_max_width = files_list.lists_column_max_width
+      total_blocks = files_list.total_blocks
 
+      puts "total #{total_blocks}"
       files.each do |file|
         print "#{file.permission.ljust(columns_max_width[:permission])} #{file.hard_link.to_s.ljust(columns_max_width[:hard_link])}  "
         print "#{file.owner_name.ljust(columns_max_width[:owner])} #{file.group_name.ljust(columns_max_width[:group])}  "
@@ -71,8 +34,8 @@ class LsCommand
         puts ''
       end
     else
-      columns = create_columns(files)
-      columns_max_width = columns_max_width(columns)
+      columns = files_list.create_columns
+      columns_max_width = files_list.columns_max_width
       columns.each do |column|
         print "#{column[0].name.ljust(columns_max_width[0])}  "
         print "#{column[1]&.name&.ljust(columns_max_width[1])}  "
